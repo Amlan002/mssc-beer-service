45d47c8f81692c7f3516241948db1225e5352cbb
Amlan002 <mahapatra.amlan11@gmail.com>
2021-11-04 23:20:04 +0530

    vy

--- src/main/java/com/beer/app/web/model/BeerDto.java
+++ src/main/java/com/beer/app/web/model/BeerDto.java
@@ -22,12 +22,12 @@ import lombok.NoArgsConstructor;
 public class BeerDto {
 	
 	@Null
-	private UUID id;
-	
-	@Null
-	private Integer version;
-	
-	@Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
